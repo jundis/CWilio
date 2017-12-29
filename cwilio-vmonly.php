24a8ae6ca35d208d6de0e477ac3f7d79d2d65719
jundis <jundis@gmail.com>
2017-12-29 12:09:54 -0600

    Remove inactives from search, can search companies now
    Cleaned up a bit, added the inactiveflag = false condition to remove
    inactive contacts from search, and added the ability for the script to
    lookup company phone numbers if it can't match to a user.

--- cwilio-vmonly.php
+++ cwilio-vmonly.php
@@ -18,7 +18,7 @@ if(array_key_exists("recorded",$_REQUEST))
     $phonenumber = substr(preg_replace('/\D+/', '', $_REQUEST['From']), 1);
 
     $header = array("Authorization: Basic " . base64_encode(strtolower($companyname) . "+" . $apipublickey . ":" . $apiprivatekey));
-    $url = $connectwise . "/v4_6_release/apis/3.0/company/contacts?childconditions=communicationItems/value%20like%20%22" . $phonenumber . "%22";
+    $url = $connectwise . "/v4_6_release/apis/3.0/company/contacts?conditions=inactiveflag%20like%20false&childconditions=communicationItems/value%20like%20%22" . $phonenumber . "%22";
 
     $ch = curl_init(); //Initiate a curl session
 
@@ -45,9 +45,6 @@ if(array_key_exists("recorded",$_REQUEST))
     $jsonDecode = json_decode($curlBodyTData); //Decode the JSON returned by the CW API.
     $jsonDecode = $jsonDecode[0];
     if (array_key_exists("code", $jsonDecode)) { //Check if array contains error code
-        if ($jsonDecode->code == "NotFound") { //If error code is NotFound
-            die("<Say>Connectwise ticket " . $_REQUEST['Digits'] . " was not found. You'll be forwarded to the help desk momentarily</Say>\n$dialhelpdesk</Response>"); //Report that the ticket was not found.
-        }
         if ($jsonDecode->code == "Unauthorized") { //If error code is an authorization error
             die("<Say>401 Unauthorized, check API key to ensure it is valid.</Say>\n</Response>"); //Fail case.
         } else {
@@ -67,7 +64,7 @@ if(array_key_exists("recorded",$_REQUEST))
     }
     else
     {
-        $subject = 'Voice message from ' . $_REQUEST['From'];
+        $subject = 'Voice message from ' . $phonenumber;
     }
 
     if ($jsonDecode != null) {
@@ -104,21 +101,82 @@ if(array_key_exists("recorded",$_REQUEST))
             "automaticEmailCcFlag" => true
         );
     } else {
-        $body = "The voice mail is attached, and a transcribed text can be found below.\r\n\r\n" . str_replace('<br />', ' ', $_REQUEST['TranscriptionText']);
+        $url = $connectwise . "/v4_6_release/apis/3.0/company/companies?conditions=phonenumber%20contains%20%22" . $phonenumber . "%22";
 
-        $postarray = array(
-            "summary" => $subject,
-            "initialInternalAnalysis" => $body,
-            "board" => array(
-                "id" => $boardID
-            ),
-            "company" => array(
-                "id" => 4
-            ),
-            "automaticEmailContactFlag" => true, // Needed due to CW known issue #8558748
-            "automaticEmailResourceFlag" => true,
-            "automaticEmailCcFlag" => true
+        $ch = curl_init(); //Initiate a curl session
+
+        //Create curl array to set the API url, headers, and necessary flags.
+        $curlOpts = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $header,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HEADER => 1,
         );
+        curl_setopt_array($ch, $curlOpts); //Set the curl array to $curlOpts
+
+        $answerTData = curl_exec($ch); //Set $answerTData to the curl response to the API.
+        $headerLen = curl_getinfo($ch, CURLINFO_HEADER_SIZE);  //Get the header length of the curl response
+        $curlBodyTData = substr($answerTData, $headerLen); //Remove header data from the curl string.
+
+        // If there was an error, show it
+        if (curl_error($ch)) {
+            die(curl_error($ch));
+        }
+        curl_close($ch);
+
+        $jsonDecode = json_decode($curlBodyTData); //Decode the JSON returned by the CW API.
+        $jsonDecode = $jsonDecode[0];
+        if (array_key_exists("code", $jsonDecode)) { //Check if array contains error code
+            if ($jsonDecode->code == "Unauthorized") { //If error code is an authorization error
+                die("<Say>401 Unauthorized, check API key to ensure it is valid.</Say>\n</Response>"); //Fail case.
+            } else {
+                die("<Say>Unknown Error Occurred, check API key and other API settings. Error: " . $jsonDecode->code . "</Say>\n</Response>"); //Fail case.
+            }
+        }
+        if (array_key_exists("errors", $jsonDecode)) //If connectwise returned an error.
+        {
+            $errors = $jsonDecode->errors; //Make array easier to access.
+
+            die("<Say>ConnectWise Error: " . $errors[0]->message . "</Say>\n</Response>"); //Return CW error
+        }
+
+        if($jsonDecode != null)
+        {
+            $body = "The voice mail is attached, and a transcribed text can be found below. This voice mail is from " . $jsonDecode->name . " though we cannot identify the user.\r\n\r\n" . str_replace('<br />', ' ', $_REQUEST['TranscriptionText']);
+
+            $postarray = array(
+                "summary" => $subject,
+                "initialInternalAnalysis" => $body,
+                "board" => array(
+                    "id" => $boardID
+                ),
+                "company" => array(
+                    "id" => $jsonDecode->id
+                ),
+                "automaticEmailContactFlag" => true, // Needed due to CW known issue #8558748
+                "automaticEmailResourceFlag" => true,
+                "automaticEmailCcFlag" => true
+            );
+        }
+        else
+        {
+            $body = "The voice mail is attached, and a transcribed text can be found below.\r\n\r\n" . str_replace('<br />', ' ', $_REQUEST['TranscriptionText']);
+
+            $postarray = array(
+                "summary" => $subject,
+                "initialInternalAnalysis" => $body,
+                "board" => array(
+                    "id" => $boardID
+                ),
+                "company" => array(
+                    "id" => 4
+                ),
+                "automaticEmailContactFlag" => true, // Needed due to CW known issue #8558748
+                "automaticEmailResourceFlag" => true,
+                "automaticEmailCcFlag" => true
+            );
+        }
     }
 
     $header = array("Authorization: Basic " . base64_encode(strtolower($companyname) . "+" . $apipublickey . ":" . $apiprivatekey), "Content-Type: application/json");
